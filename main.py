2e1eaba1064491d808f0f1edc24f2b77d4ea57fc
luis <luisinhobonetto@gmail.com>
2025-04-03 14:10:55 -0300

    incluído metodo para direcionar a cobra através das setas direcionais

--- main.py
+++ main.py
@@ -10,11 +10,18 @@ screen.tracer(0)
 
 snake = Snake()
 
+screen.listen()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.right,"Right")
+
 game_is_on = True
+
 while game_is_on:
     screen.update()
     #Aqui é onde alteramos o tempo de atualização da tela
-    time.sleep(0.30)
+    time.sleep(0.25)
     snake.move()
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -6,6 +6,7 @@ class Snake:
     def __init__(self):
         self.segments = [  ]
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
@@ -20,4 +21,17 @@ class Snake:
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
         self.segments[0].forward(MOVE_DISTANCE)
-        self.segments[0].left(90)
+        #self.segments[0].left(90)
+
+    def up(self):
+        self.head.setheading(90)
+
+    def down(self):
+        self.head.setheading(270)
+
+    def left(self):
+        self.head.setheading(180)
+
+    def right(self):
+        self.head.setheading(0)
+
