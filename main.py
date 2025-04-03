c82740c196e7db2b8c551587db552a9e65b4a69a
luis <luisinhobonetto@gmail.com>
2025-04-03 12:59:21 -0300

    primeiro commit do curso, onde todos os arquivos dentro da pasta do projeto foram adicionados ao repositório

---
+++ main.py
@@ -0,0 +1,21 @@
+from turtle import Screen,Turtle
+from snake import Snake
+import time
+
+screen = Screen()
+screen.setup(600, 600)
+screen.bgcolor("green")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.25)
+    print("hello world")
+
+    snake.move()
+
+screen.exitonclick()
